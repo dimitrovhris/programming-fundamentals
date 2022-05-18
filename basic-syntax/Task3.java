0ddc5d8ad180c0fc81e5f7a10431191f98e6cf78
Hristiyan Dimitrov <hrisit12@gmail.com>
2022-05-18 20:40:27 +0300

    Create Task3.java

---
+++ basic-syntax/Task3.java
@@ -0,0 +1,16 @@
+package basicSynatx;
+
+import java.util.Scanner;
+
+public class Basic3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double grade = Double.parseDouble(sc.nextLine());
+        if(grade >= 3.00){
+            System.out.println("Passed!");
+        }
+        else{
+            System.out.println("Failed!");
+        }
+    }
+}
