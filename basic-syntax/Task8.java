ccd98398f502626cd7ec52391e355e82da8e3628
Hristiyan Dimitrov <hrisit12@gmail.com>
2022-05-19 18:48:50 +0300

    Create Task8.java

---
+++ basic-syntax/Task8.java
@@ -0,0 +1,12 @@
+package basicSynatx;
+
+import java.util.Scanner;
+
+public class Basic8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i = 3; i <= 100; i+=3){
+            System.out.println(i);
+        }
+    }
+}
