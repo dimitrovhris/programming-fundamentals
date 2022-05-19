a7c2cf0c5faf28a09f1a788be62a192e55228c9a
Hristiyan Dimitrov <hrisit12@gmail.com>
2022-05-19 18:54:00 +0300

    Update Task9.java

--- basic-syntax/Task9.java
+++ basic-syntax/Task9.java
@@ -10,6 +10,8 @@ public class Basic9 {
         for (int i = 1; i <= n *2; i+=2){
             sum+= i;
             System.out.println(i);
+            
         }
+        System.out.printf("Sum: %d", sum);
     }
 }
