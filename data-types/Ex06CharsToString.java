c48696ae028e831e944e0fb973166bd935dbfe13
dimitrovhris <hrisit12@gmail.com>
2022-05-27 20:53:30 +0300

    Solved Task.

---
+++ data-types/Ex06CharsToString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex06CharsToString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.nextLine().charAt(0);
+        char b = sc.nextLine().charAt(0);
+        char c = sc.nextLine().charAt(0);
+        System.out.printf("%c%c%c", a, b, c);
+    }
+}
