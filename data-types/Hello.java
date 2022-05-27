6f523e54a5ee5e342b7e4def3b623888156f786f
dimitrovhris <hrisit12@gmail.com>
2022-05-27 20:09:01 +0300

    :)

--- data-types/Hello.java
+++ data-types/Hello.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello");
+        System.out.println("Helloo");
     }
 }
