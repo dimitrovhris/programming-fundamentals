c9ee0524b4e1ea71e6b104e334080d5b08290c7e
dimitrovhris <hrisit12@gmail.com>
2022-05-27 21:03:54 +0300

    Solved Task.

--- data-types/Ex07ReversedChars.java
+++ data-types/Ex07ReversedChars.java
@@ -7,6 +7,6 @@ public class Ex07ReversedChars {
         char a = sc.nextLine().charAt(0);
         char b = sc.nextLine().charAt(0);
         char c = sc.nextLine().charAt(0);
-        System.out.printf("%c %c %c", c, b, a);
+        System.out.printf("%c %c %c ", c, b, a);
     }
 }
